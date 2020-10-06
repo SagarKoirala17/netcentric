7e91b8cce3064b85f62b9678fba8991d5898fe6f
sagar805 <koiralasagar138@gmail.com>
2020-10-06 10:49:30 +0545

    fits commit

---
+++ CSITDemo/Controllers/Api/ApiController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSITDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSITDemo.Controllers.API
+{
+    [Route("api/[controller]")]//attribute route compulsory
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        public List<StudentModel> Get()
+        {
+            return GetStudentsModelsFromDb();
+        }
+        private List<StudentModel> GetStudentsModelsFromDb()
+        {
+            List<StudentModel> students = new List<StudentModel>();
+            students.Add(new StudentModel { Name = "student 1", Address = "maitidevi" });
+            students.Add(new StudentModel { Name = "student 2", Address = "balaju" });
+            students.Add(new StudentModel { Name = "student 3", Address = "biratnagar" });
+
+            return students;
+        }
+
+
+    }
+}
---
+++ CSITDemo/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSITDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSITDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = GetStudentsFromDb();
+            return View(model);
+        }
+
+        private List<StudentModel> GetStudentsFromDb()
+        {
+            List<StudentModel> students = new List<StudentModel>();
+            students.Add(new StudentModel { Name = "student 1", Address = "maitidevi" });
+            students.Add(new StudentModel { Name = "student 2", Address = "balaju" });
+            students.Add(new StudentModel { Name = "student 3", Address = "biratnagar" });
+
+            return students;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
